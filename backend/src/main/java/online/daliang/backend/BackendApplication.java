58a6604ef468cb62da5a096f26b1ecdae0dcd187
yunlianghe <yunlianghe@appannie.com>
2020-03-20 11:11:50 +0800

    add mysql conf, mapper, model and pom resource conf

--- backend/src/main/java/online/daliang/backend/BackendApplication.java
+++ backend/src/main/java/online/daliang/backend/BackendApplication.java
@@ -1,9 +1,11 @@
 package online.daliang.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "online.daliang.backend.mapper")
 public class BackendApplication {
 
     public static void main(String[] args) {
